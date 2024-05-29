7dedd9d44d27bcb23fa3685e6c96c1fc7af59283
kanzakisuemi <jsuemikanzaki@gmail.com>
2024-05-29 15:50:58 -0300

    commit final

--- app/controllers/group_reservations_controller.rb
+++ app/controllers/group_reservations_controller.rb
@@ -6,11 +6,15 @@ class GroupReservationsController < ApplicationController
   end
 
   def create
+
   end
 
   private
 
   def set_inn
     @inn = current_inn_owner.inn if inn_owner_signed_in?
+  end
+
+  def group_reservation_params
   end
 end
--- spec/system/inn_owner/inn_owner_registers_group_reservation_spec.rb
+++ spec/system/inn_owner/inn_owner_registers_group_reservation_spec.rb
@@ -36,4 +36,44 @@ describe 'Inn owner registers group reservation' do
 
     click_on 'Fazer reserva'
   end
+  it 'successfully' do
+    inn_owner = InnOwner.create!(
+      first_name: 'Julia', 
+      last_name: 'Kanzaki', 
+      document: '53783222002', 
+      email: 'kanzaki@myself.com', 
+      password: 'password123'
+    )
+    inn = inn_owner.create_inn!(
+      name: 'Pousada Kanzaki', 
+      registration_number: '30638898000200', 
+      description: 'Um bom lugar', 
+      address_attributes: { 
+        address: 'Rua Higienopolis', 
+        number: '140', 
+        city: 'Londrina', 
+        state: 'PR', 
+        postal_code: '69067-700', 
+        neighborhood: 'Centro'
+      }
+    )
+
+    login_as inn_owner, scope: :inn_owner
+
+    visit root_path
+    click_on 'Gestão de Pousadas'
+    within('section#group-reservations') do
+      click_on 'Nova reserva para grupo'
+    end
+    fill_in 'Nome', with: 'Contra da Rebase'
+    fill_in 'Data de entrada', with: 5.days.from_now
+    fill_in 'Data de saída', with: 8.days.from_now
+
+    click_on 'Fazer reserva'
+
+    expect(page).to have_content 'Reserva efetuada com sucesso'
+    expect(page).to have_content 'Contra da Rebase'
+    expect(page).to have_content 5.days.from_now
+    expect(page).to have_content 8.days.from_now
+  end
 end
